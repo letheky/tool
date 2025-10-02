4949de0e011d62322d461ecc40afe11b9c1d6fc4
Lê Thế Kỷ <kylt@aisolutionsjsc.com>
2025-10-02 16:03:55 +0700

    update map 3

--- src/views/map3.vue
+++ src/views/map3.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about" @click="handleClick">
     <div class="map">
+      <img src="/k3/map.webp" alt="" srcset="" />
       <div
         v-for="(dot, index) in clickedDots"
         :key="index"
@@ -22,11 +23,11 @@ const clickedDots = ref([]);
 const handleClick = (e) => {
   const map = document.querySelector(".map");
   const rect = map.getBoundingClientRect();
-  
+
   // Calculate position relative to the map container
   const xRelative = e.clientX - rect.left;
   const yRelative = e.clientY - rect.top;
-  
+
   const xPercent = ((xRelative / rect.width) * 100).toFixed(2);
   const yPercent = ((yRelative / rect.height) * 100).toFixed(2);
 
@@ -52,11 +53,16 @@ const handleClick = (e) => {
   .map {
     width: 75%;
     height: 100%;
-    background-image: url("/k3/map.webp");
-    background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-    position: relative;
+    img {
+      width: 120%;
+      height: 120%;
+      object-fit: contain;
+      pointer-events: none;
+      user-drag: none;
+      user-select: none;
+      transform: translate(-20rem, -20rem);
+    }
+    
   }
 
   .red-dot {
